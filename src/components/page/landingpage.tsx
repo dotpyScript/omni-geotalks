5a3d0c4454c9799b0f59ce4ce902e8a0d529948c
dotpyScript <ahlbany9@gmail.com>
2026-02-25 14:06:33 +0100

    comment some repeatative codes

--- src/components/page/landingpage.tsx
+++ src/components/page/landingpage.tsx
@@ -1,23 +1,21 @@
 import HeroSection from '@/components/sections/HeroSection';
 import WebinarDiscovery from '../sections/WebinarDiscovery';
-import AboutSection from '../sections/about/AboutSection';
-import AboutSection2 from '../sections/AboutSection';
+// import AboutSection from '../sections/about/AboutSection';
+// import AboutSection2 from '../sections/AboutSection';
 import AboutSection3 from '../sections/files3/AboutSection';
 import CategoriesSection from '../sections/files/CategoriesSection';
 import Speaker from '../sections/files1/SpeakersSection';
-import Section2 from '../sections/files2/WebinarDiscovery';
 
 export default function LandingPage() {
   return (
     <main>
       <HeroSection />
-      <WebinarDiscovery />
-      <AboutSection />
-      <AboutSection2 />
-      <CategoriesSection />
       <AboutSection3 />
+      <WebinarDiscovery />
+      {/* <AboutSection /> */}
+      {/* <AboutSection2 /> */}
+      <CategoriesSection />
       <Speaker />
-      <Section2 />
     </main>
   );
 }
